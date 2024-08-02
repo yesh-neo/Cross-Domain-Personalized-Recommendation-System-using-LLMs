d2ca4a39ce2cef8f650e78bd7da9432a0444749f
yesh-neo <yeshwanth@neohumans.ai>
2024-08-02 11:54:55 +0530

    first commit

---
+++ UI/recsys/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Welcome to Cross Domain Personalized Recommendation System using LLMs.
+      </header>
+    </div>
+  );
+}
+
+export default App;
